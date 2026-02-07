62c990dd6f5fad653c84e75960ce72c123d86a64
Farhan <ahmad.farohan@gmail.com>
2026-02-07 19:37:34 +0700

    add first python app

---
+++ PYTHON-REFRESHER/04_first_python_app/summation.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+summation = first_number + second_number
+print(f"The sum of {first_number} and {second_number} is {summation}.")
