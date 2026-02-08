fd08259e9a96c7cbd7085fd60a46048905241047
Farhan <ahmad.farohan@gmail.com>
2026-02-08 07:42:16 +0700

    add dictionaries

---
+++ PYTHON-REFRESHER/13_dictionaries/01_example.py
@@ -0,0 +1,6 @@
+friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
+
+friend_ages["Bob"] = 25
+
+print(friend_ages["Adam"])
+print(friend_ages)
---
+++ PYTHON-REFRESHER/13_dictionaries/02_example.py
@@ -0,0 +1,8 @@
+friends = [
+    {"Name": "Rolf", "Age": 24},
+    {"Name": "Adam", "Age": 30},
+    {"Name": "Anne", "Age": 27}
+]
+
+print(friends[1])
+print(friends[1]["Name"])
