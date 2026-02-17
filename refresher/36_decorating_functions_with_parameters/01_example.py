8a1be8d32c97e05cccd4812557f02f9c2f34abed
Farhan <ahmad.farohan@gmail.com>
2026-02-17 12:45:27 +0700

    add decorating functions with parameters

---
+++ refresher/36_decorating_functions_with_parameters/01_example.py
@@ -0,0 +1,21 @@
+import functools
+
+user = {"username": "jose", "access_level": "admin"} # admin / guest
+
+def make_secure(func): # make_secure is the decorator function
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if user ["access_level"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            return f"No admin permissions for {user['username']}."
+    return secure_function
+
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "admin: 1234"
+    elif panel == "billing":
+        return "super_secure_password"
+
+print(get_password("billing"))
