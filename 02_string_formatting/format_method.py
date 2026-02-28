09368bbb0f1c0f00fc23eef9c6359a2602031f24
Farhan <ahmad.farohan@gmail.com>
2026-02-28 18:04:28 +0700

    add format method and legacy percent formatting

---
+++ 02_string_formatting/format_method.py
@@ -0,0 +1,3 @@
+name = "Farhan"
+greeting = "Hello, {}!".format(name)
+print(greeting)
---
+++ 02_string_formatting/legacy_percent_formatting.py
@@ -0,0 +1,10 @@
+# Legacy Percent (%) Formatting
+
+name = "Farhan"
+greeting = "Hello, %s!" % name
+print(greeting)
+
+# Explanation:
+# In this code snippet, we are using the legacy percent (%) formatting to create a greeting message.
+# The %s is a placeholder for a string, and the value of the variable 'name' is inserted in its place.
+# The output of this code will be: "Hello, Farhan!"
