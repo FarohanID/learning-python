730f1747ee17f6ebef7f6a338c5f042b64abb55b
Farhan <ahmad.farohan@gmail.com>
2026-02-28 18:39:33 +0700

    add variable assignment and reassignment

---
+++ 01_variables/variable_assignment_and_reassignment.py
@@ -0,0 +1,12 @@
+# Example of variable assignment and reassignment in Python
+
+data = 10
+data = 20
+data = 30
+
+print(data)
+
+# Explanation:
+# In this code snippet, we are assigning the value 10 to the variable 'data', then we reassign it to 20, and finally to 30.
+# When we print 'data', it will output 30, which is the last value assigned to it.
+# This demonstrates that variables can be reassigned to new values, and the previous values are overwritten.
