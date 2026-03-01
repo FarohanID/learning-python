502de466cb29ce8cc5b5a43e1315c890c2dc6fe6
Farhan <ahmad.farohan@gmail.com>
2026-03-01 10:08:30 +0700

    add multiple variable in python

---
+++ 01_variables/multiple_variables.py
@@ -0,0 +1,11 @@
+x, y, z = "Orange", "Banana", "Cherry"
+a = b = c = "Apple"
+d, e = 5, 10
+d, e = e, d # Swap the values of d and e
+print(x) # Output: Orange
+print(y) # Output: Banana
+print(z) # Output: Cherry
+print(a) # Output: Apple
+print(b) # Output: Apple
+print(c) # Output: Apple
+print(f"a: {d}, b: {e}") # Output: a: 10, b: 5
