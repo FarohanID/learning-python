e564f39a3e038e04601d617f4c7278185c3b2978
Farhan <ahmad.farohan@gmail.com>
2026-03-12 22:23:14 +0700

    add while loop example

---
+++ 11_loops/while_loop.py
@@ -0,0 +1,4 @@
+i =  0
+while i < 10: # while loop will continue to execute as long as the condition is true
+    print(i)
+    i += 1 # this will print the numbers from 0 to 9, and then the loop will stop when i is no longer less than 10
